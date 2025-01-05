3d09fe95472d9d1483e7a3097035339d26b0d090
Mikhail Maslov <masloff.mixail2016@yandex.ru>
2025-01-05 21:09:14 +1000

    Connecting the database to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigFactory } from './config/database-config.factory';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigFactory,
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
